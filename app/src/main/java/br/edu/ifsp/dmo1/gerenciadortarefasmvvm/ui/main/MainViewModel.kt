ac1515ab89840a1f88d2bd425682225316578a60
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-25 11:15:56 -0300

    Criando ViewModel da Main.

---
+++ app/src/main/java/br/edu/ifsp/dmo1/gerenciadortarefasmvvm/ui/main/MainViewModel.kt
@@ -0,0 +1,30 @@
+package br.edu.ifsp.dmo1.gerenciadortarefasmvvm.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.data.dao.TaskDao
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.data.model.Task
+
+class MainViewModel:ViewModel() {
+    private val dao = TaskDao
+
+    private val _tasks = MutableLiveData<List<Task>>()
+    val tasks: LiveData<List<Task>>
+        get() = _tasks
+
+    init{
+        mock()
+        load()
+    }
+
+    private fun mock() {
+        dao.add(Task("Arrumar a Cama", false))
+        dao.add(Task("Retirar o lixo", false))
+        dao.add(Task("Fazer trabalho de DMO1", true))
+    }
+
+    private fun load() {
+        _tasks.value = dao.getAll()
+    }
+}
