a9ca15f0b6eea6890f899364bf1b4a67f643cde2
João Pedro Andolpho <joao.andolpho@aluno.ifsp.edu.br>
2024-11-25 11:25:27 -0300

    Atualizando o MainActivity com a lógica básica de visualização.

--- app/src/main/java/br/edu/ifsp/dmo1/gerenciadortarefasmvvm/ui/main/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/dmo1/gerenciadortarefasmvvm/ui/main/MainActivity.kt
@@ -1,21 +1,37 @@
 package br.edu.ifsp.dmo1.gerenciadortarefasmvvm.ui.main
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.R
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.databinding.ActivityMainBinding
+import br.edu.ifsp.dmo1.gerenciadortarefasmvvm.ui.adapter.TaskAdapter
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
+    private lateinit var adapter: TaskAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        configListview()
+        configObservers()
+    }
+
+    private fun configListview() {
+        adapter = TaskAdapter(this, mutableListOf())
+        binding.listTasks.adapter = adapter
+    }
+
+    private fun configObservers() {
+        viewModel.tasks.observe(this, Observer {
+            adapter.updateTasks(it)
+        })
     }
 }
